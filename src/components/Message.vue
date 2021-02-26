806bdf1ebb73b88ab36fd204836e7bf16d52b75c
Umer Hassan <umerhassan94@gmail.com>
2021-02-26 01:42:54 -0500

    improvement: added Message component to display error/info messages

---
+++ src/components/Message.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="notification" :class="types[type]">
+    <button class="delete" @click="onclick"></button>
+    Invalid zipcode, please try again!
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Message",
+  data() {
+    return {
+      types: {
+        danger: 'is-danger',
+        primary: 'is-primary',
+      }
+    }
+  },
+  props: {
+    msg: {
+      required: true,
+      type: String
+    },
+    type: {
+      type: String,
+      required: true
+    }
+  },
+  methods: {
+    onclick() {
+      this.$emit('dismiss');
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
