5fd1fb671afda31d10fb9a759f9735ee19290603
Umer Hassan <umerhassan94@gmail.com>
2021-02-26 01:44:06 -0500

    improvement: addded WeatherIcon component to display the icons with text for weather info

---
+++ src/components/WeatherIcon.vue
@@ -0,0 +1,37 @@
+<template>
+  <span :class="className">
+    <img :src="require(`@/assets/icons/${img}.svg`)"
+         class="icon" :class="{'is-small': size === 'small', 'is-medium': size === 'medium'}"
+         alt="weather image">
+    {{ text }}
+  </span>
+</template>
+
+<script>
+export default {
+  name: "WeatherIcon",
+  props: {
+    size: {
+      type: String,
+      required: false,
+      default: 'small'
+    },
+    text: {
+      type: String,
+      required: false
+    },
+    img: {
+      type: String,
+      required: true
+    },
+    className: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
